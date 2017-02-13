f1df1bf956c0b1305160f43c4a8e58430bb5a48b
Ashish Powani <ashishpowani@yahoo.com>
2017-02-12 22:37:00 -0500

    Conditionals

---
+++ Conditionals/conditionals.R
@@ -0,0 +1,21 @@
+> x=10
+> if(x==1)
++ {
++   print(x)
++ }else if(x==10)
++ {
++   print("ten")
++ }else
++ {
++   print("nothing")
++ }
+[1] "ten"
+
+  
+> ifelse(x<5,"yes","no")
+[1] "no"
+> ifelse(x<5,0,1)
+[1] 1
+  
+  
+  
