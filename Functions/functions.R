58c2d787ef7b6ba1cca881253541518155534ff8
Ashish Powani <ashishpowani@yahoo.com>
2017-04-02 14:25:03 -0400

    Update functions.R

--- Functions/functions.R
+++ Functions/functions.R
@@ -1,13 +1,13 @@
 #create a new function
-> mySum<-function(x,y)
+mySum<-function(x,y)
 + {
 +   return(x+y)
 + }
-> mySum(3,4)
+mySum(3,4)
 [1] 7
 
 #get body of a function - just type the name of the function
-> mySum
+mySum
 function(x,y)
 {
   return(x+y)
